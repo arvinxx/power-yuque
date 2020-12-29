626eb8cf424f5eea57f3948383aab918d92ddfc2
Arvin Xu <arvinx@foxmail.com>
2020-12-29 15:18:50 +0800

    Initial commit

---
+++ config/config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  locale: { default: 'zh-CN' },
+  extensions: {
+    name: 'Umi Chrome Extension Template',
+    description: '基于 Umi 的 Chrome 插件开发脚手架',
+    optionsUI: {
+      page: '@/pages/options',
+      openInTab: true,
+    },
+    background: { scripts: ['@/background/index'] },
+    popupUI: '@/pages/popup',
+    icons: {
+      16: 'logo/logo@16.png',
+      32: 'logo/logo@32.png',
+      48: 'logo/logo@48.png',
+      128: 'logo/logo@128.png',
+    },
+  },
+});
