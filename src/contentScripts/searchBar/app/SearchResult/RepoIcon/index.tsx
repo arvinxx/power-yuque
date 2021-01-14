00bbcaf010b914ac2f0c26b016086a651df8b381
arvinxx <arvinx@foxmail.com>
2021-01-15 03:45:51 +0800

    ✨ feat: 添加资源库 icon

--- src/contentScripts/searchBar/app/SearchResult/RepoIcon/index.tsx
+++ src/contentScripts/searchBar/app/SearchResult/RepoIcon/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ReactComponent as BookSvg } from './book.svg';
 import { ReactComponent as ColumnSvg } from './column.svg';
 import { ReactComponent as DesignSvg } from './design.svg';
+import { ReactComponent as ResourceSvg } from './resource.svg';
 
 const RepoIcon: FC<{ type: string }> = ({ type }) => {
   switch (type) {
@@ -12,6 +13,8 @@ const RepoIcon: FC<{ type: string }> = ({ type }) => {
       return <DesignSvg />;
     case 'column':
       return <ColumnSvg />;
+    case 'resource':
+      return <ResourceSvg />;
     default:
       return null;
   }
