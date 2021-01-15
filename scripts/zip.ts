162754af72fff2e28eef59cd7bd8926e3ca75cb4
arvinxx <arvinx@foxmail.com>
2021-01-15 13:55:28 +0800

    🔧 chore: 添加压缩脚本

---
+++ scripts/zip.ts
@@ -0,0 +1,53 @@
+#!/usr/bin/env ts-node
+
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+
+const extPackageJson = require('../package.json');
+
+const DEST_DIR = path.join(__dirname, '../dist');
+const DEST_ZIP_DIR = path.join(__dirname, '../dist-zip');
+
+const extractExtensionData = () => {
+  return {
+    name: extPackageJson.name,
+    version: extPackageJson.version,
+  };
+};
+
+const makeDestZipDirIfNotExists = () => {
+  if (!fs.existsSync(DEST_ZIP_DIR)) {
+    fs.mkdirSync(DEST_ZIP_DIR);
+  }
+};
+
+const buildZip = (src: string, dist: string, zipFilename: string) => {
+  console.info(`Building ${zipFilename}...`);
+
+  const archive = archiver('zip', { zlib: { level: 9 } });
+  const stream = fs.createWriteStream(path.join(dist, zipFilename));
+
+  return new Promise<void>((resolve, reject) => {
+    archive
+      .directory(src, false)
+      .on('error', (err: any) => reject(err))
+      .pipe(stream);
+
+    stream.on('close', () => resolve());
+    archive.finalize();
+  });
+};
+
+const main = () => {
+  const { name, version } = extractExtensionData();
+  const zipFilename = `${name}-v${version}.zip`;
+
+  makeDestZipDirIfNotExists();
+
+  buildZip(DEST_DIR, DEST_ZIP_DIR, zipFilename)
+    .then(() => console.info('OK'))
+    .catch(console.error);
+};
+
+main();
