f597c3e7f1bfbb4e9435a449c0c91acdfceb32ae
arvinxx <arvinx@foxmail.com>
2021-01-16 23:12:33 +0800

    💄 styles: 完成团队页面样式修改

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'umi';
 import { resolve } from 'path';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
   locale: { default: 'zh-CN' },
   alias: {
@@ -8,7 +10,7 @@ export default defineConfig({
   },
   // chrome 插件配置项
   extensions: {
-    name: 'Power Yuque',
+    name: `Power Yuque${isDev ? ' DEV' : ''}`,
     description: '语雀增强插件',
     optionsUI: {
       page: '@/pages/options',
