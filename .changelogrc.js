8d0a1048ca50c8382d5aba2fddd6a1e758b4e176
arvinxx <arvinx@foxmail.com>
2021-01-18 22:03:32 +0800

    :wrench: chore: add auto release

---
+++ .changelogrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  scopeDisplayName: {
+    changelog: 'conventional-changelog-gitmoji-config',
+    config: 'commitlint-config-gitmoji',
+    'parser-opts': '@gitmoji/parser-opts',
+    'commit-type': '@gitmoji/commit-types',
+    'commit-types': '@gitmoji/commit-types',
+    '*': '杂项',
+  },
+  displayTypes: ['feat', 'fix', 'styles', 'pref'],
+};
