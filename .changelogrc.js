d9d9f47a91ca9a6ac60d3921f94c3fc593ee719f
arvinxx <arvinx@foxmail.com>
2021-01-19 05:09:00 +0800

    :package: chore(dependency): 更新依赖配置项

--- .changelogrc.js
+++ .changelogrc.js
@@ -1,10 +1,5 @@
 module.exports = {
   scopeDisplayName: {
-    changelog: 'conventional-changelog-gitmoji-config',
-    config: 'commitlint-config-gitmoji',
-    'parser-opts': '@gitmoji/parser-opts',
-    'commit-type': '@gitmoji/commit-types',
-    'commit-types': '@gitmoji/commit-types',
     '*': '杂项',
   },
   displayTypes: ['feat', 'fix', 'styles', 'pref'],
