3549c0bc49a25d022b68bbeeea22354089545f3d
arvinxx <arvinx@foxmail.com>
2021-01-19 03:12:32 +0800

    :fire: chore: clean code

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/runner/work/power-yuque/power-yuque/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','locale','locale','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/runner/work/power-yuque/power-yuque/node_modules/react-helmet';
