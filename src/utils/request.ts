396e9e20c0345a5938178e623b3f15ff05bda84f
arvinxx <arvinx@foxmail.com>
2021-01-21 12:33:35 +0800

    :bug: fix(search-bar): 修复无 token 时搜索框无法显示问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -60,6 +60,6 @@ export const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
   headers: {
     'X-Auth-Token':
-      localStorage.getItem('yuque_token')!.replace(/"/g, '') || '',
+      localStorage.getItem('yuque_token')?.replace(/"/g, '') || '',
   },
 });
