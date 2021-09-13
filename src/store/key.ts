57c89b2cc4385c118b3e08d75a1c0ba94317b65b
arvinxx <arvinx@foxmail.com>
2021-09-14 06:48:39 +0800

    :art: build: 修正配置项

--- src/store/key.ts
+++ src/store/key.ts
@@ -13,16 +13,13 @@ export const PY_KEYS = {
    * 语雀主题
    */
   theme: 'PY_THEME',
-
-  /* 热力图 */
-  heatmap: {
+  config: {
     /**
-     * 热力图数据
+     *
      */
-    data: 'PY_HEATMAP_RAW_DATA',
-    /**
-     * 热力图更新日期
-     */
-    updatedAt: 'PY_HEATMAP_UPDATED_AT',
+    typography: {
+      enable: 'PY_CONFIG_TYPOGRAPHY_ENABLE',
+      noSpacing: 'PY_CONFIG_TYPOGRAPHY_NO_SPACING',
+    },
   },
 };
